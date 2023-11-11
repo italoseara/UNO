8b40bc1391c182b18235a57766dac05381754745
Italo Seara <italo.sseara@gmail.com>
2023-11-11 00:39:43 -0300

    feat: random cards in credits

--- src/core/graphics/gfx.py
+++ src/core/graphics/gfx.py
@@ -1,6 +1,20 @@
+import random
 import pygame
+import os
 
 
 class Gfx:
+    LOGO = pygame.image.load("src/assets/images/logo.png")
     MENU_BACKGROUND = pygame.image.load("src/assets/images/background/menu.png")
     CREDITS_BACKGROUND = pygame.image.load("src/assets/images/background/credits.png")
+
+    @staticmethod
+    def random_card() -> pygame.Surface:
+        files = os.listdir("src/assets/images/cards")
+        file = random.choice(files)
+
+        # Load image with 500% scale
+        image = pygame.image.load(f"src/assets/images/cards/{file}")
+        image = pygame.transform.scale(image, (int(image.get_width() * 5), int(image.get_height() * 5)))
+
+        return image
